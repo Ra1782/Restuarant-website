ec59caae99bcfe261ffd1a389bbe86c99a6d78e2
Rohan Asrani <61574943+Ra1782@users.noreply.github.com>
2020-09-13 15:24:44 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
+
+function App() {
+  return (
+    <div>
+      <Navbar dark color="secondary" >
+        <div className="container">
+          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+        </div>
+      </Navbar>
+      <Menu />
+    </div>
+  );
+}
+
+export default App;

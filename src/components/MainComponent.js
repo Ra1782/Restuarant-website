6ed4171a31cd031d386f0a571c328ed58a92013a
Rohan Asrani <rohanasrani13@gmail.com>
2020-09-20 22:33:38 +0530

    Styling

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './MenuComponent';
 import { DISHES } from '../shared/dishes';
 import DishDetail from './DishDetailComponent.js';
+import Header from './HeaderComponent.js';
+import Footer from './FooterComponent.js';
 
 class Main extends Component{
   constructor(props){
@@ -19,13 +20,10 @@ class Main extends Component{
   render(){
     return (
       <div>
-        <Navbar light color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
+        <Header />
         <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
         <DishDetail selectedDish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+        <Footer />
       </div>
     );
   }

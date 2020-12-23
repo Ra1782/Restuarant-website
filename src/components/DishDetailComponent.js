ee02dbb735e98e18147f02dfe0e2c4e34a7b98be
Rohan Asrani <rohanasrani13@gmail.com>
2020-12-23 16:12:36 +0530

    Last assignment done with warnings resolved

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {Card, CardImg, CardBody, CardTitle, CardText, Breadcrumb, BreadcrumbItem, 
-    Button, Modal, ModalBody, ModalHeader, Label, Row, Col, Container} from 'reactstrap';
+    Button, Modal, ModalBody, ModalHeader, Label, Row, Col} from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,16 +8,17 @@ import Contact from './ContactComponent';
 import About from './AboutComponent';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { postComment, fetchDishes, fetchComments, fetchPromos } from '../redux/ActionCreators';
-import { actions } from 'react-redux-form';
+import { postComment, fetchDishes, fetchComments, fetchPromos, fetchLeaders, postFeedbackForm } from '../redux/ActionCreators';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 const mapDispatchToProps = dispatch => ({
   postComment: (dishId, rating, author, comment) => dispatch(postComment(dishId, rating, author, comment)),
   fetchDishes: () => { dispatch(fetchDishes())},
-  resetFeedbackForm: () => { dispatch(actions.reset('feedback'))},
+  postFeedbackForm: (firstname, lastname, telnum, email, agree, contactType, message) => 
+  { dispatch(postFeedbackForm(firstname, lastname, telnum, email, agree, contactType, message)) },
   fetchComments: () => dispatch(fetchComments()),
-  fetchPromos: () => dispatch(fetchPromos())
+  fetchPromos: () => dispatch(fetchPromos()),
+  fetchLeaders: () => dispatch(fetchLeaders())
 });
 
 const mapStateToProps = (state) => {
@@ -30,14 +31,11 @@ const mapStateToProps = (state) => {
 };
 
 class Main extends Component{
-  constructor(props){
-    super(props);
-  }
-
   componentDidMount() {
     this.props.fetchDishes();
     this.props.fetchComments();
     this.props.fetchPromos();
+    this.props.fetchLeaders();
   }
   
   render(){
@@ -50,7 +48,9 @@ class Main extends Component{
               promotion={this.props.promotions.promotions.filter((promo) => promo.featured)[0]}
               promoLoading={this.props.promotions.isLoading}
               promoErrMess={this.props.promotions.errMess}
-              leader={this.props.leaders.filter((leader) => leader.featured)[0]}
+              leader={this.props.leaders.leaders.filter((leader) => leader.featured)[0]}
+              leadersLoading={this.props.leaders.isLoading}
+              leadersErrMess={this.props.leaders.errMess}
           />
         );
     }
@@ -76,7 +76,7 @@ class Main extends Component{
               <Route path="/home" component={HomePage}/>
               <Route exact path="/menu" component={() => <Menu dishes={this.props.dishes}/>}/>
               <Route path="/menu/:dishId" component={DishWithId} />
-              <Route exact path="/contactus" component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm}/>} />
+              <Route exact path="/contactus" component={() => <Contact postFeedbackForm={this.props.postFeedbackForm}/>} />
               <Route exact path="/aboutus" component={() => <About leaders={this.props.leaders} />} />
               <Redirect to="/home"/>
             </Switch>
